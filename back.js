7748c4678c802142a32b094e11168c3c491ff6e3
cesarcoelho-dev <cesarhenriquecoelho@icloud.com>
2024-01-24 13:01:04 -0300

    push

---
+++ back.js
@@ -0,0 +1,21 @@
+function insert(num) {
+    var numero = document.getElementById('result').innerHTML;
+    document.getElementById('result').innerHTML = numero + num;
+}   
+function clean(){
+    document.getElementById('result').innerHTML = "";
+}
+
+function back(){
+    var resultado = document.getElementById('result').innerHTML;
+    document.getElementById('result').innerHTML = resultado.substring(0, resultado.length -1);
+}
+
+function calcular(){
+    var resultado = document.getElementById('result').innerHTML;
+    if(result){
+        document.getElementById('result').innerHTML = eval(resultado);
+    }else {
+        document.getElementById('result').innerHTML = "unfefined";
+    }
+}
